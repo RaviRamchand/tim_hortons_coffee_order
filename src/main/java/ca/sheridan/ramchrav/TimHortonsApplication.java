2778a91492cbcc1abe7aa1b50c84b434ecfb0d2b
Ravi <raviramchand8@gmail.com>
2021-12-30 16:52:11 -0500

    contains main method

---
+++ src/main/java/ca/sheridan/ramchrav/TimHortonsApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridan.ramchrav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimHortonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimHortonsApplication.class, args);
+    }
+
+}
