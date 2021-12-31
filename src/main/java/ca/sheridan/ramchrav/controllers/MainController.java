a4f33aa2b80fbf2958e9c377f2a199d1f6b4d7af
Ravi <raviramchand8@gmail.com>
2021-12-31 16:19:19 -0500

    added List of Orders

--- src/main/java/ca/sheridan/ramchrav/controllers/MainController.java
+++ src/main/java/ca/sheridan/ramchrav/controllers/MainController.java
@@ -1,14 +1,19 @@
 package ca.sheridan.ramchrav.controllers;
 
 import ca.sheridan.ramchrav.beans.Orders;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 @Controller
 public class MainController {
+    List<Orders> oList = new CopyOnWriteArrayList<Orders>();
 
     @GetMapping("/")
     public String getIndex(Model model){
@@ -17,10 +22,10 @@ public class MainController {
     }
 
     @PostMapping("/getOrder")
-    public String getOrder(@ModelAttribute Orders orders){
-        System.out.println(orders.getQuantity());
-        System.out.println(orders.getSize());
-        System.out.println(orders.getCoffeeType());
+    public String getOrder(Model model, @ModelAttribute Orders orders){
+        oList.add(orders);
+        model.addAttribute("oList", oList);
+        //model.addAttribute("order", orders);
         return "orders";
     }
 
